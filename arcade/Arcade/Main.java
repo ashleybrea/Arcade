d5b1af81638ed01836db51b94bf8183e73c3cfba
Ashley Brea ୨୧ <134354106+ashleyb1705@users.noreply.github.com>
2024-06-03 01:23:37 -0400

    Delete arcade directory

--- arcade/Arcade/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-
-public class Main {
-    AGame [] arcade = new AGame[4];
-    Arrays.sort(arcade);
-
-    System.out.println("Welcome to Ashley's arcade!" + "\n" + "Pick the game you'd like to play :)");
-    System.out.println("");
-}
